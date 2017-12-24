302ab4d1a17b485885df117bc6c73dbf08079d75
yurov <iurovrs@gmail.com>
2017-12-24 22:04:36 +0300

    Compile-time list: is element in list

--- main.cpp
+++ main.cpp
@@ -5,12 +5,12 @@ struct Int {
     static constexpr int32_t value = v;
 };
 
-struct NIL {
-    typedef NIL Head;
-    typedef NIL Tail;
+struct Nil {
+    typedef Nil Head;
+    typedef Nil Tail;
 };
 
-template<class H, class T = NIL>
+template<class H, class T = Nil>
 struct List {
     typedef H Head;
     typedef T Tail;
@@ -23,7 +23,7 @@ struct Length {
 };
 
 template<>
-struct Length<NIL> {
+struct Length<Nil> {
     static constexpr uint32_t length = 0;
 };
 
@@ -35,13 +35,34 @@ using Tail = typename List::Tail;
 
 template<class List, uint32_t N>
 struct Nth {
-	typedef Tail<List> tail;
-	typedef class Nth<tail, N-1>::value value;
+	typedef Tail<List> CurTail;
+	typedef class Nth<CurTail, N-1>::value value;
 };
 
 template<class List>
 struct Nth<List, 0> {
 	typedef Head<List> value;
+};
+
+template<class X, class Y>
+struct Equal {
+	static constexpr bool result = false;
+};
+
+template<class X>
+struct Equal<X, X> {
+	static constexpr bool result = true;
+};
+
+template<class Elem, class List>
+struct IsInList {
+	static constexpr bool result = Equal<Elem, Head<List>>::result ||
+			IsInList<Elem, Tail<List>>::result;
+};
+
+template<class Elem>
+struct IsInList<Elem, Nil> {
+	static constexpr bool result = false;
 };
 
 int main() {
@@ -51,5 +72,6 @@ int main() {
     std::cout << "List head: " << Head<list>::value << std::endl;
     std::cout << "List second element: " << Head<Tail<list>>::value << std::endl;
     std::cout << "List third element: " << Nth<list, 2>::value::value << std::endl;
+    std::cout << "List third element: " << IsInList<Int<3>, list>::result << std::endl;
     return 0;
 }
